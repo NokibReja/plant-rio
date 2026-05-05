ad4592136c34614660f8c273cd5aae0c6e93ba66
NokibReja <you@example.com>
2026-05-06 01:08:19 +0600

    define home layout

---
+++ src/layouts/HomeLayouts.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import Header from '../components/Header/Header';
+
+const HomeLayouts = () => {
+    return (
+        <div>
+            <header>
+                <Navbar></Navbar>
+                <Header></Header>
+            </header>
+        </div>
+    );
+};
+
+export default HomeLayouts;
